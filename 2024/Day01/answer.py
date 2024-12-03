b0c6b9d8d7e309ddf8cc2fe3f213430a12642535
daniel-havely <114996231+daniel-havely@users.noreply.github.com>
2024-12-03 13:33:23 +0000

    Tidier version

--- 2024/Day01/answer.py
+++ 2024/Day01/answer.py
@@ -1,15 +1,10 @@
 with open("input.txt","r") as input_file:
     input_data = [[int(num) for num in number_string.split()] for number_string in input_file.read().split("\n") if number_string]
 
-location_lists = [list(tup) for tup in zip(*input_data)]
-
-for col in location_lists:
-    col.sort()
+location_lists = [sorted(list(tup)) for tup in zip(*input_data)]
 
 distances = [abs(a-b) for a,b in zip(*location_lists)]
-
-print("Total distance  ",sum(distances),sep="\t")
-
 similarity_coeffs = [loc*location_lists[1].count(loc) for loc in location_lists[0]]
 
+print("Total distance  ",sum(distances),sep="\t")
 print("Total similarity",sum(similarity_coeffs),sep="\t")
